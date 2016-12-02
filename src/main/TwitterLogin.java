2dea500c5b775457ef0f395480566d6016bef916
veivan <veivan@yandex.ru>
2016-12-02 14:30:43 +0300

    Logged in but not post

--- src/main/TwitterLogin.java
+++ src/main/TwitterLogin.java
@@ -8,15 +8,26 @@ import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
 import java.net.CookieHandler;
 import java.net.CookieManager;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.config.CookieSpecs;
+import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.protocol.HttpClientContext;
+import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.client.LaxRedirectStrategy;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.Header;
 import org.apache.http.HttpResponse;
@@ -27,7 +38,7 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 public class TwitterLogin {
-	private final String USER_AGENT = "Mozilla/5.0";
+	private static final String USER_AGENT = "Mozilla/5.0";
 	private String cookies;
 	private String twitter_sess;
 	private String guest_id;
@@ -36,7 +47,20 @@ public class TwitterLogin {
 	private String auth_token;
 	private String twid;
 
-	private HttpClient client = HttpClientBuilder.create().build();
+	/*
+	 * private HttpClient client = HttpClientBuilder.create()
+	 * .setRedirectStrategy(new LaxRedirectStrategy()).build();
+	 */
+
+	 private HttpClient client = HttpClientBuilder.create().setUserAgent(USER_AGENT).build();
+
+/*	 private static final CloseableHttpClient client = HttpClients
+			.custom()
+			.setUserAgent(USER_AGENT)
+			.setDefaultRequestConfig(
+					RequestConfig.custom()
+							.setCookieSpec(CookieSpecs.BROWSER_COMPATIBILITY)
+							.build()).build(); */
 
 	public static void main(String[] args) {
 		String urlLogin = "https://twitter.com/login";
@@ -48,18 +72,16 @@ public class TwitterLogin {
 
 		TwitterLogin http = new TwitterLogin();
 
-		/*
-		 * try { http.sendPostfirst(urlLogin); } catch (Exception e1) { // TODO
-		 * Auto-generated catch block e1.printStackTrace(); }
-		 */
-
 		String result = "";
 		try {
 			String page = http.GetPageContent(urlLogin);
-			
-			List<NameValuePair> postParams = http.getFormParams(page, "user",	"pass");
+
+			List<NameValuePair> postParams = http.getFormParams(page,
+					"u", "p");
 			http.sendPost(urlsess, postParams);
-			result = http.GetPageContent(urlmain);
+			http.dotwit("Hi-people");
+
+			// result = http.GetPageContent(urlmain);
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -70,95 +92,70 @@ public class TwitterLogin {
 		System.out.println("Done");
 	}
 
-	private void sendPostfirst(String url) throws Exception {
-
-		HttpPost post = new HttpPost(url);
-
-		// add header
-		post.setHeader("Host", "twitter.com");
-		post.setHeader("User-Agent", USER_AGENT);
-		post.setHeader("Accept",
-				"text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8");
-		post.setHeader("Accept-Language",
-				"ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4,bg;q=0.2");
-		post.setHeader("Cookie", getCookies());
-		post.setHeader("Connection", "keep-alive");
-		post.setHeader("Referer", "https://twitter.com/");
-		post.setHeader("Content-Type", "application/x-www-form-urlencoded");
-
-		// post.setEntity(new UrlEncodedFormEntity(postParams));
-
-		HttpResponse response = client.execute(post);
-
-		int responseCode = response.getStatusLine().getStatusCode();
-
-		System.out.println("\nSending 'POST' request to URL : " + url);
-		System.out.println("Request : " + post.toString());
-		// System.out.println("Post parameters : " + postParams);
-		System.out.println("Response Code : " + responseCode);
-		System.out.println("Response : " + response.toString());
-
-		BufferedReader rd = new BufferedReader(new InputStreamReader(response
-				.getEntity().getContent()));
-
-		StringBuffer result = new StringBuffer();
-		String line = "";
-		while ((line = rd.readLine()) != null) {
-			result.append(line);
-		}
+	private void Save2file(String buffer, String filename) throws Exception {
 		BufferedWriter bwr = new BufferedWriter(new FileWriter(new File(
-				"d:/demo.txt")));
+				filename)));
 		// write contents of StringBuffer to a file
-		bwr.write(result.toString());
+		bwr.write(buffer);
 		// flush the stream
 		bwr.flush();
 		// close the stream
 		bwr.close();
-		// System.out.println(result.toString());
-
-		System.out.println("Cookies : "
-				+ collectCookiesresponse(response.getHeaders("set-cookie")));
-
 	}
 
 	private void sendPost(String url, List<NameValuePair> postParams)
 			throws Exception {
 
-		HttpPost post = new HttpPost(url);
+ 		//CloseableHttpResponse response = null;
+		
+		HttpResponse response = null;
+		
+		try {
+			HttpPost post = new HttpPost(url);
 
-		// add header
-		post.setHeader("Host", "twitter.com");
-		post.setHeader("User-Agent", USER_AGENT);
-		post.setHeader("Accept",
-				"text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8");
-		post.setHeader("Accept-Language",
-				"ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4,bg;q=0.2");
-		post.setHeader("Cookie", getCookies());
-		post.setHeader("Connection", "keep-alive");
-		post.setHeader("Referer",
-				"https://twitter.com/download?logged_out=1&lang=ru");
-		post.setHeader("Content-Type", "application/x-www-form-urlencoded");
+			// add header
+			post.setHeader("Host", "twitter.com");
+			//post.setHeader("User-Agent", USER_AGENT);
+			post.setHeader("Accept",
+					"text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8");
+			post.setHeader("Accept-Language",
+					"ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4,bg;q=0.2");
+			post.setHeader("Cookie", getCookies());
+			post.setHeader("Connection", "keep-alive");
+			post.setHeader("Referer", "https://twitter.com");
+			post.setHeader("Content-Type", "application/x-www-form-urlencoded");
 
-		post.setEntity(new UrlEncodedFormEntity(postParams, "UTF-8"));
+			post.setEntity(new UrlEncodedFormEntity(postParams, "UTF-8"));
 
-		HttpResponse response = client.execute(post);
+			response = client.execute(post);
 
-		int responseCode = response.getStatusLine().getStatusCode();
+			int responseCode = response.getStatusLine().getStatusCode();
 
-		System.out.println("\nSending 'POST' request to URL : " + url);
-		System.out.println("Post parameters : " + postParams);
-		System.out.println("Response Code : " + responseCode);
+			System.out.println("\nSending 'POST' request to URL : " + url);
+			System.out.println("Post parameters : " + postParams);
+			System.out.println("Response Code : " + responseCode);
 
-		BufferedReader rd = new BufferedReader(new InputStreamReader(response
-				.getEntity().getContent()));
+			BufferedReader rd = new BufferedReader(new InputStreamReader(
+					response.getEntity().getContent()));
 
-		StringBuffer result = new StringBuffer();
-		String line = "";
-		while ((line = rd.readLine()) != null) {
-			result.append(line);
+			StringBuffer result = new StringBuffer();
+			String line = "";
+			while ((line = rd.readLine()) != null) {
+				result.append(line);
+			}
+
+			Save2file(result.toString(), "d:/demo.txt");
+           
+            String cooky = collectCookiesresponse(response
+					.getHeaders("set-cookie"));
+			System.out.println(cooky);
+		} finally {
+			/*if (response != null) {
+				response.close();
+			} */
 		}
 
-		//System.out.println(result.toString());
+		// System.out.println(result.toString());
 
 	}
 
@@ -194,7 +191,16 @@ public class TwitterLogin {
 		return result.toString();
 
 	}
+	
+	private void dotwit(String statusbody) throws Exception {
 
+		List<NameValuePair> postParams = new ArrayList<NameValuePair>();
+		postParams.add(new BasicNameValuePair("status",
+				URLEncoder.encode(statusbody, "UTF-8")));		
+		
+		sendPost("https://api.twitter.com/1.1/statuses/update.json", postParams);
+	}
+	
 	private String collectCookiesresponse(Header[] headers) {
 		StringBuilder result = new StringBuilder();
 		for (Header header : headers) {
@@ -214,11 +220,12 @@ public class TwitterLogin {
 		Document doc = Jsoup.parse(html);
 
 		// Login form id
-		Element loginform = doc.getElementsByClass("LoginForm js-front-signin").first();// getElementById("LoginForm.js-front-signin");
+		Element loginform = doc.getElementsByClass("LoginForm js-front-signin")
+				.first();// getElementById("LoginForm.js-front-signin");
 		Elements inputElements = loginform.getElementsByTag("input");
 
 		List<NameValuePair> paramList = new ArrayList<NameValuePair>();
-		
+
 		for (Element inputElement : inputElements) {
 			String key = inputElement.attr("name");
 			String value = inputElement.attr("value");
@@ -233,12 +240,14 @@ public class TwitterLogin {
 			paramList.add(new BasicNameValuePair(key, value));
 
 		}
-		/*paramList.add(new BasicNameValuePair("session[username_or_email]",
-				URLEncoder.encode(username, "UTF-8")));
-		paramList.add(new BasicNameValuePair("session[password]", URLEncoder
-				.encode(password, "UTF-8")));
-
-		paramList.add(new BasicNameValuePair("remember_me", "0")); */
+		/*
+		 * paramList.add(new BasicNameValuePair("session[username_or_email]",
+		 * URLEncoder.encode(username, "UTF-8"))); paramList.add(new
+		 * BasicNameValuePair("session[password]", URLEncoder .encode(password,
+		 * "UTF-8")));
+		 * 
+		 * paramList.add(new BasicNameValuePair("remember_me", "0"));
+		 */
 
 		/*
 		 * return_to_ssl:true
